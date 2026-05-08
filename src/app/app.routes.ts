cd3dd6a2c2f4af59605f7a493a6880fb5cacf063
renansantanax <renan.2r@live.com>
2026-05-08 08:55:50 -0300

    Criação da página de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pages/login',
+    component: Login,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'pages/login',
+  },
+];
